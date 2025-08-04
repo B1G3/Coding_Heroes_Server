f7a8323e66871797d64fce69738619f8ba1940ac
binmuxiz <binmuxiz@gmail.com>
2025-08-04 21:21:25 +0900

    .

--- config.py
+++ config.py
@@ -3,10 +3,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.join(BASE_DIR, "data")
+OUTPUT_DIR = os.path.join(BASE_DIR, "outputs")
 
-PROMPT_PATH = os.path.join(DATA_DIR, "system_prompt", "prompt_0729_v1.txt")
+PROMPT_PATH = os.path.join(DATA_DIR, "system_prompt", "prompt_0804.txt")
 
-DOCS_PATH = os.path.join(DATA_DIR, 'rag_docs', 'docs_0729_v1.md')
+# RAG 문서 경로들
+RAG_DOCS_DIR = os.path.join(DATA_DIR, 'rag_docs')
 
 CHROMA_DB_PATH = os.path.join(DATA_DIR, 'chroma_db')
 DB_PATH = os.path.join(DATA_DIR, "coding-heroes.db")
--- core/services.py
+++ core/services.py
@@ -14,47 +14,23 @@ stt = STT(parameters={})
 stt.load_model()
 tts = TTS()
 
-
-
-# stt
 async def speech_to_text(audio_file: UploadFile):
     try:
         await audio_file.seek(0)  
         audio_data = await audio_file.read()
         
-        result = await stt.run(audio_data)
-        return result
+        result_text = await stt.run(audio_data)
+        return result_text
 
     except Exception as e:
         logging.error(f"STT 처리 중 오류 발생: {str(e)}")
         raise
 
 
-
-# llm 응답
-def get_ai_response(question: str, user_id: str, conversation_id: str):
-    return chat(user_question=question, 
-                user_id=user_id, 
-                conversation_id=conversation_id)
-
-
-# tts
 async def text_to_speech(text: str):
     try:
-        # 2. TTS 처리 (LLM 답변을 음성으로 변환)
-        audio = tts.run(text, "pcm_16000")
-        audio_bytes = b''.join(audio)
-        b64_data = base64.b64encode(audio_bytes).decode("utf-8")
-
-        # output 디렉토리에 저장 (나중에 주석처리하기)
-        output_dir = "./outputs"
-        os.makedirs(output_dir, exist_ok=True)  # 디렉토리 없으면 생성
-
-        timestamp = datetime.now().strftime("%m%d_%H%M")
-        filename = os.path.join(output_dir, f"audio_{timestamp}.wav")
-
-        save_pcm_as_wav(audio_bytes, filename)
-
+        audio_content = await tts.run(text)
+        b64_data = base64.b64encode(audio_content).decode("utf-8")
 
         return b64_data
     
@@ -63,12 +39,9 @@ async def text_to_speech(text: str):
         raise
 
 
-import wave
-def save_pcm_as_wav(pcm_data: bytes, filename: str, sample_rate=16000):
-    with wave.open(filename, 'wb') as wav_file:
-        wav_file.setnchannels(1)         # mono
-        wav_file.setsampwidth(2)         # 16-bit
-        wav_file.setframerate(sample_rate)
-        wav_file.writeframes(pcm_data)   # write raw PCM data
-
-    print(f"저장 완료: {filename}")
+    
+# llm 응답
+def get_ai_response(question: str, user_id: str, conversation_id: str):
+    return chat(user_question=question, 
+                user_id=user_id, 
+                conversation_id=conversation_id)
