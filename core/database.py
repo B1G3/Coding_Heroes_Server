c9c4a12595050c6b39d75d2a4caab16508201cbc
binmuxiz <binmuxiz@gmail.com>
2025-08-06 22:36:24 +0900

    [Add] 플레이기록 추가, 조회, 사용자 추가

--- core/database.py
+++ core/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from core.models import Base, User, Message
+from core.models import Base, User, Message, PlayRecord
 
 
 import logging
@@ -18,8 +18,20 @@ SessionLocal = sessionmaker(bind=engine)
 # ----------------- main.py에서 서버 실행시 한 번 호출 ---------------
 def init_db():
     Base.metadata.create_all(bind=engine)
-# 테이블이 존재하지 않으면 새로 생성
 
+
+def save_user(user_id: str, username: str):
+    db = SessionLocal()
+    try:
+        user = User(
+            user_id=user_id,
+            username=username
+        )
+        db.add(user)
+        db.commit()
+
+    finally:
+        db.close()
 
 
 
@@ -48,7 +60,7 @@ def select_messages_by_user_and_conversation_id(user_id: str, conversation_id: s
 
 
 
-def add_message(user_id: str, conversation_id: str, role: str, content: str):
+def save_message(user_id: str, conversation_id: str, role: str, content: str):
     """
     대화 내용을 데이터베이스에 저장하는 함수
     """
@@ -69,16 +81,64 @@ def add_message(user_id: str, conversation_id: str, role: str, content: str):
 
 
 
-def delete_messages_by_ids(message_ids: list):
+def delete_messages_by_ids(ids: list):
     """
     메시지 id 리스트를 받아 해당 메시지들을 삭제하는 함수
     """
     db = SessionLocal()
     try:
-        db.query(Message).filter(Message.id.in_(message_ids)).delete(synchronize_session=False)
+        db.query(Message).filter(Message.id.in_(ids)).delete(synchronize_session=False)
         db.commit()
     finally:
         db.close()
 
 
 
+def delete_playrecord(ids: list):
+    db = SessionLocal()
+    try:
+        db.query(PlayRecord).filter(PlayRecord.id.in_(ids)).delete(synchronize_session=False)
+        db.commit()
+    finally:
+        db.close()
+
+
+
+def save_playrecord(user_id: str, stage: str, json_str: str):
+    """
+    플레이어의 게임 이용 결과를 저장하는 함수
+    """
+    db = SessionLocal()
+    try:
+        coding_result = PlayRecord(
+            user_id=user_id,
+            json_str=json_str,
+            stage=stage
+        )
+        db.add(coding_result)
+        db.commit()
+    finally:
+        db.close()
+
+
+def select_playrecord(user_id: str, stage: str):
+    """
+    가장 최신 플레이 기록으로 가져오기
+    """
+    db = SessionLocal()
+    try:
+        json_str = (
+            db.query(PlayRecord.json_str)
+            .filter_by(
+                user_id=user_id,
+                stage=stage
+            ).order_by(PlayRecord.timestamp.desc())
+            .scalar() # 가장 최신 항목 하나만 가져옴
+
+        )
+        logger.info(f"json_str => {json_str}")
+
+        return json_str or ""
+
+    finally:
+        db.close()
