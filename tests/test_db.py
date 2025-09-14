1f66747f2964512ebaad07cd28ceacdff4db96bb
binmuxiz <binmuxiz@gmail.com>
2025-09-15 03:45:21 +0900

    Chore: Refactor temporary data for testing

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,50 +1,28 @@
-from core.database import init_db, delete_messages_by_ids
 
 base_url = "https://jolly-accepted-bonefish.ngrok-free.app"
 
 
-from core.models import Base, User, Message
 import requests
 
-from core.database import save_user
-from core.database import delete_playrecord
 
-def save_sample_user_test(user_id, username):
-    save_user(user_id, username)
-
-
-def save_playrecord_test(stage, block_data):
-    endpoint = "ai_npc/playrecord"
+def save_execution_log_test(stage, block_json_str):
+    endpoint = "ai_npc/execution_log"
     url = f"{base_url}/{endpoint}"
-
-    import json
-    json_str = json.dumps(block_data, ensure_ascii=False)
 
     payload = {
         "stage": stage,
-        "json_str": json_str
+        "block_json": block_json_str
     }
     response = requests.post(url, json=payload)
 
     print("Status Code", response.status_code)
-    print("Response", response.json())
+    # print("Response", response.json())
 
-
-
-def delete_playrecord_api_test(ids: list):
-    delete_playrecord(ids)
 
 
 if __name__ == "__main__":
-    # save_sample_user("u001", "은빈빈")
-
-    # playrecord_ids = [1]
-    # delete_playrecord_api_test(playrecord_ids)
-
-
-        # JSON 구조를 Python dict로 먼저 선언
-    stage = "1"
-    block_data = {
+    stage = "learn"
+    block_json = {
         "line": [
             {
                 "block_id": 1,
@@ -54,35 +32,20 @@ if __name__ == "__main__":
             },
             {
                 "block_id": 2,
+                "type": "ACTION",
+                "name": "이동한다",
+                "next": 3
+            },
+            {
+                "block_id": 3,
                 "type": "UNIT",
                 "name": "백신 멍멍이"
             }
         ]
     }
 
-    save_playrecord_test(stage, block_data)
+    import json
+    block_json_str = json.dumps(block_json, ensure_ascii=False)
+    print(block_json_str)
 
-
-            # {
-            #     "block_id": 1,
-            #     "type": "UNIT",
-            #     "name": "Start",
-            #     "next": 2
-            # },
-            # {
-            #     "block_id": 2,
-            #     "type": "ACTION",
-            #     "name": "공격한다",
-            #     "next": 4
-            # },
-            # {
-            #     "block_id": 3,
-            #     "type": "ACTION",
-            #     "name": "이동한다",
-            #     "next": 4
-            # },
-            # {
-            #     "block_id": 4,
-            #     "type": "UNIT",
-            #     "name": "백신 멍멍이"
-            # }
+    save_execution_log_test(stage, block_json_str)
